a03b036efc0b758a7ac28ab7b4e2d3a16c496ea2
Nils Meijer <meijernils9@gmail.com>
2023-12-12 15:32:51 +0100

    Refactoring. Replaced font

--- Javascript/MainScene.js
+++ Javascript/MainScene.js
@@ -1,54 +1,31 @@
 import * as THREE from 'three';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 import PortfolioContent from './PortfolioContent.js';
 import TerminalProperties from './TerminalProperties.js';
-
-// const fs = require('fs');
-// const path = require('path');
+import TerminalFrontEnd from './TerminalFrontEnd.js';
+import TerminalBackEnd from './TerminalBackEnd.js';
 
 export default class MainScene extends THREE.Scene {
-    fontLoader;
-    inputFieldTextObject;
-    inputFieldContent = "";
-    inputFieldGroup;
-    terminalContentGroup;
     terminalProps;
+    frontend;
+    backend;
 
     currentDirectory = "";
     currentCommand = "";
     portfolioContent;
-    cursorVisible = true;
-    caretTick;
     backgroundPlane
     userIsTyping;
 
     constructor() {
         super()
-        this.fontLoader = new FontLoader();
-        this.inputFieldGroup = new THREE.Group();
-        this.terminalContentGroup = new THREE.Group();
         this.terminalProps = new TerminalProperties();
+        this.frontend = new TerminalFrontEnd(this);
+        this.frontend.scene = this;
+        this.frontend.properties = this.terminalProps;
 
-        this.add(this.inputFieldGroup);
-        this.add(this.terminalContentGroup);
+        this.backend = new TerminalBackEnd();
 
-        this.inputFieldGroup.position.set(-6, -3.5, 0);
-        this.terminalContentGroup.position.set(-6, -2.5, 0);
-        this.inputFieldContent = this.terminalProps.defaultTerminalLine;
-
-        //this.addToTerminalContent(this.asciiArt, 0.2);
-        this.addToTerminalContent(this.terminalProps.customDefaultText[0])
-        this.addToTerminalContent(this.terminalProps.customDefaultText[1])
-        this.addToTerminalContent("Enter 'help' to show a list of available commands.")
         this.currentDirectory = this.terminalProps.defaultTerminalLine;
         this.portfolioContent = new PortfolioContent();
-        this.createCursorTick();
-        setInterval(() => {
-            this.loopCursorTick();
-        }, 600);
-        this.updateInputField();
-        this.createBackground();
 
         const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
         directionalLight.position.set(1, 1, 1).normalize();
@@ -62,19 +39,19 @@ export default class MainScene extends THREE.Scene {
         const key = event.key;
         if (key == "Backspace") {
             event.preventDefault();
-            if (this.inputFieldContent.endsWith(">")) {
+            if (this.frontend.inputFieldContent.endsWith(">")) {
                 console.log("Current character is >")
                 return;
             }
 
-            const newContent = this.inputFieldContent.slice(0, -1);
-            this.inputFieldContent = newContent;
-            this.updateInputField();
+            const newContent = this.frontend.inputFieldContent.slice(0, -1);
+            this.frontend.inputFieldContent = newContent;
+            this.frontend.updateInputField();
             return;
         }
 
         if (key == "Enter") {
-            let trimmedString = this.inputFieldContent.trim();
+            let trimmedString = this.frontend.inputFieldContent.trim();
             if (trimmedString == this.currentDirectory)
                 return;
             this.submitContent();
@@ -83,29 +60,12 @@ export default class MainScene extends THREE.Scene {
         if (this.terminalProps.specialKeys.includes(key))
             return;
 
-        this.inputFieldContent += key;
-        this.updateInputField();
+        this.frontend.inputFieldContent += key;
+        this.frontend.updateInputField();
     }
 
     onDocumentKeyRelease() {
         this.userIsTyping = false;
-    }
-
-    createCursorTick() {
-        const cursorGeometry = new THREE.BoxGeometry(0.02, 0.4, 0.02);
-        const cursorMaterial = new THREE.MeshStandardMaterial({ color: 0x000000, emissive: new THREE.Color(0xffffff), emissiveIntensity: 1000 });
-        this.caretTick = new THREE.Mesh(cursorGeometry, cursorMaterial);
-        this.inputFieldGroup.add(this.caretTick);
-        this.caretTick.position.set(0.6, 0.1, 0);
-    }
-
-    createBackground() {
-        const backgroundGeometry = new THREE.BoxGeometry(100, 0.4, 100);
-        const backgroundMaterial = new THREE.MeshPhongMaterial({ color: 0x322222 });
-        this.backgroundPlane = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
-        this.backgroundPlane.position.set(0.6, 0.1, -0.1);
-        this.backgroundPlane.rotateOnAxis(new THREE.Vector3(1, 0, 0), 90);
-        this.terminalContentGroup.add(this.backgroundPlane);
     }
 
     checkIfCapitalLetter(event, character) {
@@ -115,7 +75,7 @@ export default class MainScene extends THREE.Scene {
     }
 
     submitContent() {
-        this.addToTerminalContent(this.inputFieldContent);
+        this.frontend.addToTerminalContent(this.frontend.inputFieldContent);
 
         let isValid = this.checkValidInput();
         this.resetTerminalLine();
@@ -148,87 +108,41 @@ export default class MainScene extends THREE.Scene {
         }
     }
 
-
-    addToTerminalContent(textGiven, customFontSize = 0.2) {
-        let newLine;
-        this.fontLoader.load(
-            './static/fonts/helvetiker_regular.typeface.json',
-            (font) => {
-                const textGeometry = new TextGeometry(textGiven, {
-                    font,
-                    size: customFontSize,
-                    height: 0.01,
-                });
-                newLine = new THREE.Mesh(textGeometry);
-
-                this.terminalContentGroup.traverse(function (object) {
-                    if (object.type != "Group")
-                        object.position.y += 0.35;
-                });
-
-                this.terminalContentGroup.add(newLine);
-            });
-    }
-
     resetTerminalLine() {
-        this.inputFieldContent = this.terminalProps.defaultTerminalLine + this.terminalProps.addedToTerminalPath;
-        this.updateInputField();
-    }
-
-    updateInputField() {
-        this.inputFieldGroup.remove(this.inputFieldTextObject);
-        this.fontLoader.load(
-            './static/fonts/helvetiker_regular.typeface.json',
-            (font) => {
-                const textGeometry = new TextGeometry(this.inputFieldContent, {
-                    font,
-                    size: 0.2,
-                    height: 0.01,
-                });
-                this.inputFieldTextObject = new THREE.Mesh(textGeometry);
-                this.inputFieldGroup.add(this.inputFieldTextObject);
-                this.inputFieldTextObject.updateMatrixWorld();
-
-                this.inputFieldTextObject.geometry.computeBoundingBox();
-                const boundingBox = this.inputFieldTextObject.geometry.boundingBox;
-                const centerX = (boundingBox.min.x + boundingBox.max.x) / 2;
-                const defaultOffset = 0.05;
-                this.caretTick.position.x = centerX + (boundingBox.max.x / 2) + defaultOffset;
-            });
+        this.frontend.inputFieldContent = this.terminalProps.defaultTerminalLine + this.terminalProps.addedToTerminalPath;
+        this.frontend.updateInputField();
     }
 
     checkValidInput() {
         //Find the last >.
-        let bracketIndex = this.inputFieldContent.indexOf(">");
+        let bracketIndex = this.frontend.inputFieldContent.indexOf(">");
         let resultString = "";
         if (bracketIndex == -1) {
             return false;
         }
 
         //Retrieve the full input
-        resultString = this.inputFieldContent.substring(bracketIndex + 1).trim();
+        resultString = this.frontend.inputFieldContent.substring(bracketIndex + 1).trim();
 
         //Retrieve the command
-        this.currentCommand = resultString.split(" ")[0];
+        let command = resultString.split(" ")[0];
+        this.currentCommand = command.toLowerCase();
+
         //If array does not contain command, it is invalid. Throw error message to terminal.
         if (!this.terminalProps.validCommandsMap.has(this.currentCommand)) {
+            console.log()
             this.addToTerminalContent("'" + this.currentCommand + "'" + this.terminalProps.errorMessageInvalidCommand);
             return false;
         }
 
+        //If the command is "dir" or "help", input is valid and we should not check for any path.
         if (this.currentCommand.toLowerCase() == "dir" || this.currentCommand.toLowerCase() == "help")
             return true;
 
         this.givenDirectory = resultString.split(" ")[1];
         //Path is not valid.
         if (this.givenDirectory == undefined || this.givenDirectory == "") {
-            this.addToTerminalContent(this.errorMessageInvalidDirectory);
-            return false;
-        }
-
-        //Check if path exists.
-        if (!this.terminalProps.validLayer1Directories.includes()) {
-            this.addToTerminalContent(this.errorMessageInvalidDirectory);
+            //this.addToTerminalContent(this.errorMessageInvalidDirectory);
             return false;
         }
 
@@ -241,8 +155,8 @@ export default class MainScene extends THREE.Scene {
             const data = await response.json();
 
             if (import.meta.env.MODE === 'development') {
-                console.log('Directories:', data.directories);
-                console.log('files:', data.files);
+                // console.log('Directories:', data.directories);
+                // console.log('files:', data.files);
             }
 
             const dirArray = Object.values(data.directories);
@@ -269,14 +183,15 @@ export default class MainScene extends THREE.Scene {
     }
 
     executeCommand() {
+        console.log("command:", this.currentCommand);
         switch (this.currentCommand) {
             case "help":
-                this.addToTerminalContent("Valid commands are:")
+                this.frontend.addToTerminalContent("Valid commands are:")
                 const map = this.terminalProps.validCommandsMap;
                 const keyArray = Array.from(map.keys())
                 const valueArray = Array.from(map.values());
                 for (let i = 0; i < keyArray.length; i++) {
-                    this.addToTerminalContent("     " + keyArray[i] + " - " + valueArray[i]);
+                    this.frontend.addToTerminalContent("     " + keyArray[i] + " - " + valueArray[i]);
                 }
                 break;
             //List all subdirectories of current root directory.
@@ -284,16 +199,16 @@ export default class MainScene extends THREE.Scene {
                 (async () => {
                     try {
                         const data = await this.recursivelySearchDirectories();
-                        
+
                         //Directories
-                        this.addToTerminalContent("Subdirectories of " + this.currentDirectory);
+                        this.frontend.addToTerminalContent("Subdirectories of " + this.currentDirectory);
                         const dirArray = data.directories.map(directory => {
                             // Replace everything before "/Dir" with an empty string
                             return directory.replace(/.*\/Terminal/, '');
                         });
                         for (let i = 0; i < dirArray.length; i++) {
                             let dirName = dirArray[i];
-                            this.addToTerminalContent(" - " + dirName);
+                            this.frontend.addToTerminalContent(" - " + dirName);
                         }
 
                         //Files
@@ -313,36 +228,10 @@ export default class MainScene extends THREE.Scene {
                 break;
             //Move into specified directory.
             case "cd":
-                // let newDirectory = this.recursivelySearchDirectories();
-                // if (newDirectory == "invalid") {
-                //     return;
-                // }
-                this.moveIntoDirectory(newDirectory);
+                console.log(this.currentCommand);
+                this.addToTerminalContent(this.errorMessageInvalidDirectory);
                 break;
             case "cat":
-                break;
-        }
-    }
-
-    loopCursorTick() {
-        switch (this.cursorVisible) {
-            case true:
-                this.enableCursorTick(false);
-                break;
-            case false:
-                this.enableCursorTick(true);
-                break;
-        }
-        this.cursorVisible = !this.cursorVisible;
-    }
-
-    enableCursorTick(state) {
-        switch (state) {
-            case true:
-                this.inputFieldGroup.remove(this.caretTick);
-                break;
-            case false:
-                this.inputFieldGroup.add(this.caretTick);
                 break;
         }
     }
---
+++ Javascript/TerminalFrontEnd.js
@@ -0,0 +1,150 @@
+import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
+export default class TerminalFrontEnd {
+    scene;
+    fontLoader;
+    loadedFont;
+    inputFieldGroup;
+    terminalContentGroup;
+    cursorVisible = true;
+    caretTick;
+    properties;
+    inputFieldContent = "";
+    inputFieldTextObject;
+
+    constructor(scene) {
+        this.scene = scene;
+        this.fontLoader = new FontLoader();
+        
+        this.inputFieldGroup = new THREE.Group();
+        this.terminalContentGroup = new THREE.Group();
+        
+        this.inputFieldGroup.position.set(-9.3, -4.2, 0);
+        this.terminalContentGroup.position.set(-9.3, -3.7, 0);
+
+        this.scene.add(this.inputFieldGroup);
+        this.scene.add(this.terminalContentGroup);
+
+        this.createCursorTick();
+        this.createBackground();
+
+        (async () => {
+            try {
+                await this.loadFont();
+                this.createTerminal();
+            } catch (error) {
+                throw (error);
+            }
+        })();
+    }
+
+    async loadFont() {
+        try {
+            this.loadedFont = await new Promise((resolve, reject) => {
+                this.fontLoader.load('./static/fonts/hack.json', (font) => {
+                    resolve(font);
+                }, undefined, reject);
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    createTerminal() {
+        this.inputFieldContent = this.properties.defaultTerminalLine;
+
+        //this.addToTerminalContent(this.asciiArt, 0.2);
+        this.addToTerminalContent(this.properties.customDefaultText[0])
+        this.addToTerminalContent(this.properties.customDefaultText[1])
+        this.addToTerminalContent("Enter 'help' to show a list of available commands.")
+
+        setInterval(() => {
+            this.loopCursorTick();
+        }, 600);
+        this.updateInputField();
+    }
+
+    createBackground() {
+        const backgroundGeometry = new THREE.BoxGeometry(100, 0.4, 100);
+        const backgroundMaterial = new THREE.MeshPhongMaterial({ color: 0x322222 });
+        this.backgroundPlane = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
+        this.backgroundPlane.position.set(0.6, 0.1, -0.1);
+        this.backgroundPlane.rotateOnAxis(new THREE.Vector3(1, 0, 0), 90);
+        this.terminalContentGroup.add(this.backgroundPlane);
+    }
+
+    addToTerminalContent(textGiven, customFontSize = 0.12) {
+        let newLine;
+
+        if (textGiven == undefined) {
+            console.log("text given is not valid.")
+            return;
+        }
+        const textGeometry = new TextGeometry(textGiven, {
+            font: this.loadedFont,
+            size: customFontSize,
+            height: 0.01,
+        });
+        newLine = new THREE.Mesh(textGeometry);
+
+        this.terminalContentGroup.traverse(function (object) {
+            if (object.type != "Group")
+                object.position.y += 0.28;
+        });
+
+        this.terminalContentGroup.add(newLine);
+    }
+
+    createCursorTick() {
+        const cursorGeometry = new THREE.BoxGeometry(0.02, 0.4, 0.02);
+        const cursorMaterial = new THREE.MeshStandardMaterial({ color: 0x000000, emissive: new THREE.Color(0xffffff), emissiveIntensity: 1000 });
+        this.caretTick = new THREE.Mesh(cursorGeometry, cursorMaterial);
+        this.inputFieldGroup.add(this.caretTick);
+        this.caretTick.position.set(0.6, 0.1, 0);
+    }
+
+    loopCursorTick() {
+        switch (this.cursorVisible) {
+            case true:
+                this.enableCursorTick(false);
+                break;
+            case false:
+                this.enableCursorTick(true);
+                break;
+        }
+        this.cursorVisible = !this.cursorVisible;
+    }
+
+    enableCursorTick(state) {
+        switch (state) {
+            case true:
+                this.inputFieldGroup.remove(this.caretTick);
+                break;
+            case false:
+                this.inputFieldGroup.add(this.caretTick);
+                break;
+        }
+    }
+
+    updateInputField() {
+        if(this.inputFieldTextObject != undefined)
+            this.inputFieldGroup.remove(this.inputFieldTextObject);
+
+        const textGeometry = new TextGeometry(this.inputFieldContent, {
+            font: this.loadedFont,
+            size: 0.2,
+            height: 0.01,
+        });
+        this.inputFieldTextObject = new THREE.Mesh(textGeometry);
+        this.inputFieldGroup.add(this.inputFieldTextObject);
+        this.inputFieldTextObject.updateMatrixWorld();
+
+        this.inputFieldTextObject.geometry.computeBoundingBox();
+        const boundingBox = this.inputFieldTextObject.geometry.boundingBox;
+        const centerX = (boundingBox.min.x + boundingBox.max.x) / 2;
+        const defaultOffset = 0.05;
+        this.caretTick.position.x = centerX + (boundingBox.max.x / 2) + defaultOffset;
+    }
+}
