9b553f22a6c91cbdd2eb521e4477de34c61736b7
Nils Meijer <meijernils9@gmail.com>
2023-12-12 08:52:27 +0100

    Fixed path syntax error

--- PHP/ListAllDirectories.php
+++ PHP/ListAllDirectories.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 
 // Get the directory path from the query parameter
-$directoryPath = __DIR__ . 'C:';
+$directoryPath = __DIR__ . '/C:';
 
 // Get the list of directories
 $directories = array_filter(glob($directoryPath . '/*'), 'is_dir');
