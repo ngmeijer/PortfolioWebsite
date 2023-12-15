9f27ab2b730d9afbaf6d5e5bb0a3b098007f2126
Nils Meijer <meijernils9@gmail.com>
2023-12-15 08:53:48 +0100

    Added security to server communication (allowed origin sources)

--- PHP/CheckDirectory.php
+++ PHP/CheckDirectory.php
@@ -1,16 +1,21 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+$allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://nilsmeijer.com'];
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
 
-$rootDirectory = __DIR__ . '/MainDrive/';
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header('Access-Control-Allow-Credentials: true');
+}
+
+$rootDirectory = __DIR__ . "/MainDrive/";
 
 // Get the directory path from the query parameter
 $dataFromJavascript = isset($_GET['data']) ? $_GET['data'] : '';
+$finalPath = $rootDirectory . $dataFromJavascript;
 
-echo "coming from php";
-
-if (is_dir("/". $dataFromJavascript)) {
-    echo "true";
+if (is_dir($finalPath)) {
+    echo "true. Valid path: " . $finalPath;
 } else {
-    echo "false";
+    echo "false. Invalid path: " . $finalPath;
 }
 ?>
--- PHP/ListAllDirectories.php
+++ PHP/ListAllDirectories.php
@@ -1,8 +1,14 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+$allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://nilsmeijer.com'];
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header('Access-Control-Allow-Credentials: true');
+}
 
 // Get the directory path from the query parameter
-$path = isset($_GET['path']) ? $_GET['path'] :'';
+$path = isset($_GET['path']) ? $_GET['path'] : '';
 
 // Get the list of directories
 $directories = array_filter(glob($path . '/*'), 'is_dir');
@@ -10,7 +16,7 @@ $directories = array_filter(glob($path . '/*'), 'is_dir');
 // Get files
 $files = array_filter(glob($path . '/*'), 'is_file');
 
-$result = ['directories' => $directories, 'files' => $files];
+$result = ['directories' => $directories, 'files' => $files, 'path' => $path];
 
 echo json_encode($result);
 ?>
